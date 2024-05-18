a2c92fca5afa693f34f8f0b5a8ce0a0c031f5530
jarethholt <jareth.holt@gmail.com>
2024-05-18 20:27:46 +0200

    feat: Add Entity classes

---
+++ TournamentsAPI.Core/Entities/Game.cs
@@ -0,0 +1,12 @@
+﻿namespace TournamentsAPI.Core.Entities;
+
+public class Game
+{
+    // Core properties
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public DateTime Time { get; set; }
+
+    // Navigation property
+    public int TournamentId { get; set; }
+}
---
+++ TournamentsAPI.Core/Entities/Tournament.cs
@@ -0,0 +1,12 @@
+﻿namespace TournamentsAPI.Core.Entities;
+
+public class Tournament
+{
+    // Core properties
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public DateTime StartDate { get; set; }
+
+    // Navigation property
+    public ICollection<Game> Games { get; set; } = [];
+}
