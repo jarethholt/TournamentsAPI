e76b54ef3e74af2e351d19a60be3c16398fcad44
jarethholt <jareth.holt@gmail.com>
2024-05-20 11:53:11 +0200

    fix: Spelling error and missing date in SeedData

--- TournamentsAPI.Data/Data/SeedData.cs
+++ TournamentsAPI.Data/Data/SeedData.cs
@@ -14,15 +14,14 @@ public static class SeedData
             new() { Title = "Semifinal 3-1/1-2 (Denmark) vs 4-1/2-2 (Norway)", Time = new(2023, 12, 15, 17, 30, 00) },
             new() { Title = "Semifinal 1-1/3-2 (Sweden) vs 2-1/4-2 (France)", Time = new(2023, 12, 15, 21, 00, 00) },
             new() { Title = "Final 1-1/2-1/3-2/4-2 (France) vs 1-2/2-2/3-1/4-1 (Norway)", Time = new(2023, 12, 17, 19, 00, 00) } ] },
-        new() { Title = "2023 Worls Men's Handball Championship", StartDate = new(), Games = [
+        new() { Title = "2023 World Men's Handball Championship", StartDate = new(2023, 01, 11, 00, 00, 00), Games = [
             new() { Title = "Quarterfinal 4-1 (Denmark) vs 2-2 (Hungary)", Time = new(2023, 01, 25, 18, 00, 00) },
             new() { Title = "Quarterfinal 3-1 (Norway) vs 1-2 (Spain)", Time = new(2023, 01, 25, 18, 00, 00) },
             new() { Title = "Quarterfinal 2-1 (Sweden) vs 4-2 (Egypt)", Time = new(2023, 01, 25, 20, 30, 00) },
             new() { Title = "Quarterfinal 1-1 (France) vs 3-2 (Germany)", Time = new(2023, 01, 25, 20, 54, 00) },
             new() { Title = "Semifinal 3-1/1-2 (Spain) vs 4-1/2-2 (Denmark)", Time = new(2023, 01, 27, 18, 00, 00) },
             new() { Title = "Semifinal 1-1/3-2 (France) vs 2-1/4-2 (Sweden)", Time = new(2023, 01, 27, 21, 00, 00) },
-            new() { Title = "Final 1-1/2-1/3-2/4-2 (France) vs 1-2/2-2/3-1/4-1 (Denmark)", Time = new(2023, 01, 29, 21, 00, 00) },
-            ]}
+            new() { Title = "Final 1-1/2-1/3-2/4-2 (France) vs 1-2/2-2/3-1/4-1 (Denmark)", Time = new(2023, 01, 29, 21, 00, 00) } ] }
     ];
 
     public static async Task SeedDataAsync(TournamentsContext context)
